8fc275933c24bc52a4ae6417260fb75ea8d0f22d
Dell <somayamahmoud30@gmail.com>
2024-10-14 23:10:27 +0300

    add cubit

---
+++ lib/presentation/view_models/verify_email_view_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:injectable/injectable.dart';
+import 'package:online_exam_app/domain/common/ApiResult.dart';
+import 'package:online_exam_app/domain/uses_cases/verify_code_use_case.dart';
+
+@injectable
+class VerifyEmailViewModel extends Cubit<VerifyEmailState> {
+  VerifyCodeUseCase verifyCodeUseCase;
+  VerifyEmailViewModel(this.verifyCodeUseCase)
+      : super(VerifyEmailInitialState());
+  void verifyEmailCode(String resetCode) async {
+    emit(VerifyEmailLoadingState());
+    var result = await verifyCodeUseCase.invoke(resetCode);
+    switch (result) {
+      case Success():
+        {
+          emit(VerifyEmailSuccessState());
+        }
+      case Fail():
+        {
+          emit(VerifyEmailErrorState(result.exception));
+        }
+      case null:
+    }
+  }
+}
+
+sealed class VerifyEmailState {}
+
+class VerifyEmailInitialState extends VerifyEmailState {}
+
+class VerifyEmailLoadingState extends VerifyEmailState {}
+
+class VerifyEmailErrorState extends VerifyEmailState {
+  Exception? exception;
+  VerifyEmailErrorState(this.exception);
+}
+
+class VerifyEmailSuccessState extends VerifyEmailState {}
